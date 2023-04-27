cc1d689f60554d232ffd133553db1fa592894073
mulyant20 <mulyan.t20@gmail.com>
2023-04-27 14:43:37 +0700

    3. path parameter

--- routes.js
+++ routes.js
@@ -13,6 +13,23 @@ const routes = [
       return 'homepage'
     },
   },
+  {
+    method: 'GET',
+    path: '/book/{name}',
+    handler: (req, h) => {
+      const { name } = req.params
+      return `new arrival book ${name}`
+    },
+  },
+  // optional path parameter
+  {
+    method: 'GET',
+    path: '/upcoming-book/{name?}',
+    handler: (req, h) => {
+      const {name} = req.params
+      return `upcoming book ${name}`
+    }
+  }
 ]
 
 module.exports = routes
